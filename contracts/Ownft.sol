a8ef97cc17a11e93b6d7a87b0945259e408357ab
xiaohuo <xiaohuo200@gmail.com>
2021-12-28 21:23:44 +0800

    feat: add IERC20 and _safeTransfer

--- contracts/Ownft.sol
+++ contracts/Ownft.sol
@@ -1,5 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity  ^0.8.9;
 
-contract Ownft {
+import './libraries/ownership/Ownable.sol';
+import './libraries/utils/Address.sol';
 
+contract Ownft is Ownable {
+    using Address for address;
+
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
+
+
+    mapping(address => bool) _depositWhitelist;
+
+    constructor() public {}
+
+    function _safeTransfer(
+        address token,
+        address to,
+        uint value
+    ) private {
+       (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+       require(success && (data.length == 0 || abi.decode(data, (bool))), 'Ownft: TRANSFER_FAILED');
+    }
+
+    // set up assets that can be deposited
+    function setDepositWhiteList(
+        address token,
+        bool enable
+    ) onlyOwner public returns(uint) {
+        _depositWhitelist[token] = enable;
+        return 0;
+    }
+
+    function deposit(
+        address token,
+        uint amount
+    ) public {
+
+    }
 }
