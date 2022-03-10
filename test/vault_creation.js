1f742b6e464bffd44bcef285e2e44358cfc771fb
xiaohuo <xiaohuo200@gmail.com>
2022-03-10 19:49:38 +0800

    test: uncomment test cases

--- test/vault_creation.js
+++ test/vault_creation.js
@@ -52,9 +52,7 @@ describe("Vault Creation", function () {
     it("Create Vault should return a valid vault contract", async function () {
 
         // create vault
-        const tx = await voyager.createVault();
-        console.log(tx);
-
+        await voyager.createVault();
         const vaultAddress = await vaultStorage.getVaultAddress(owner.address);
         const Vault = await ethers.getContractFactory("Vault");
         const vault = Vault.attach(vaultAddress);
@@ -62,46 +60,46 @@ describe("Vault Creation", function () {
 
     })
 
-    // it("Created Vault should have own a valid escrow contract", async function () {
-    //
-    //     const [owner] = await ethers.getSigners();
-    //     // create vault
-    //     await voyager.createVault();
-    //
-    //     const vaultAddress = await vaultStorage.getVaultAddress(owner.address);
-    //     const Vault = await ethers.getContractFactory("Vault");
-    //     const vault = Vault.attach(vaultAddress);
-    //
-    //     const SecurityDepositEscrow = await ethers.getContractFactory("SecurityDepositEscrow");
-    //     const securityDepositEscrowAddress = await vault.getSecurityDepositEscrowAddress();
-    //     const securityDepositEscrow = SecurityDepositEscrow.attach(securityDepositEscrowAddress);
-    //     expect(await securityDepositEscrow.getVersion()).to.equal("SecurityDepositEscrow 0.0.1");
-    //
-    // })
+    it("Created Vault should have own a valid escrow contract", async function () {
 
-    // it("Security deposit should return correct value", async function () {
-    //
-    //     const [owner] = await ethers.getSigners();
-    //     // create vault
-    //     await voyager.createVault();
-    //
-    //     const vaultAddress = await vaultStorage.getVaultAddress(owner.address);
-    //     const Vault = await ethers.getContractFactory("Vault");
-    //     const vault = Vault.attach(vaultAddress);
-    //     const securityDepositEscrowAddress = await vault.getSecurityDepositEscrowAddress();
-    //
-    //     // deploy mock tus contract
-    //     const Tus = await ethers.getContractFactory("Tus");
-    //     const tus = await Tus.deploy("1000000000000000000000");
-    //     await tus.increaseAllowance(securityDepositEscrowAddress, "10000000000000000000000");
-    //
-    //     await vault.depositSecurity(tus.address, "10000000000000000000");
-    //     const SecurityDepositEscrow = await ethers.getContractFactory("SecurityDepositEscrow");
-    //     const securityDepositEscrow = SecurityDepositEscrow.attach(securityDepositEscrowAddress);
-    //     const depositAmount = await securityDepositEscrow.getDepositAmount(tus.address, owner.address);
-    //     expect(depositAmount).to.equal("10000000000000000000");
-    //
-    // })
+        const [owner] = await ethers.getSigners();
+        // create vault
+        await voyager.createVault();
+
+        const vaultAddress = await vaultStorage.getVaultAddress(owner.address);
+        const Vault = await ethers.getContractFactory("Vault");
+        const vault = Vault.attach(vaultAddress);
+
+        const SecurityDepositEscrow = await ethers.getContractFactory("SecurityDepositEscrow");
+        const securityDepositEscrowAddress = await vault.getSecurityDepositEscrowAddress();
+        const securityDepositEscrow = SecurityDepositEscrow.attach(securityDepositEscrowAddress);
+        expect(await securityDepositEscrow.getVersion()).to.equal("SecurityDepositEscrow 0.0.1");
+
+    })
+
+    it("Security deposit should return correct value", async function () {
+
+        const [owner] = await ethers.getSigners();
+        // create vault
+        await voyager.createVault();
+
+        const vaultAddress = await vaultStorage.getVaultAddress(owner.address);
+        const Vault = await ethers.getContractFactory("Vault");
+        const vault = Vault.attach(vaultAddress);
+        const securityDepositEscrowAddress = await vault.getSecurityDepositEscrowAddress();
+
+        // deploy mock tus contract
+        const Tus = await ethers.getContractFactory("Tus");
+        const tus = await Tus.deploy("1000000000000000000000");
+        await tus.increaseAllowance(securityDepositEscrowAddress, "10000000000000000000000");
+
+        await vault.depositSecurity(tus.address, "10000000000000000000");
+        const SecurityDepositEscrow = await ethers.getContractFactory("SecurityDepositEscrow");
+        const securityDepositEscrow = SecurityDepositEscrow.attach(securityDepositEscrowAddress);
+        const depositAmount = await securityDepositEscrow.getDepositAmount(tus.address, owner.address);
+        expect(depositAmount).to.equal("10000000000000000000");
+
+    })
 
 });
 
