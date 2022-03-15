083000525f0e003bb54b6761af0f9b22eddef007
xiaohuo <xiaohuo200@gmail.com>
2022-03-15 13:53:14 +0800

    test(staking): refactor setup

--- contracts/component/staking/StakingRewards.sol
+++ contracts/component/staking/StakingRewards.sol
@@ -47,7 +47,7 @@ contract StakingRewards is
         _;
     }
 
-    constructor(address _stakingToken, address _rewardsToken) public Ownable() {
+    constructor(address _stakingToken, address _rewardsToken) {
         stakingToken = IERC20(_stakingToken);
         rewardsToken = IERC20(_rewardsToken);
     }
--- test/staking.js
+++ test/staking.js
@@ -1,11 +1,33 @@
 const { expect } = require("chai");
 
-// describe("Staking contract", function () {
-//     it("Deployment should give the deployer the ownership", async function () {
-//         const [owner] = await ethers.getSigners();
-//         const StakingRewards = await ethers.getContractFactory("StakingRewards");
-//         const fakeAddress = "0xf693248F96Fe03422FEa95aC0aFbBBc4a8FdD172";
-//         const stakingRewards = await StakingRewards.deploy(fakeAddress, fakeAddress,fakeAddress);
-//         expect(await stakingRewards.owner()).to.equal(owner.address);
-//     });
-// });
+let owner;
+let tus;
+let securityDepositToken;
+let stakingRewards;
+
+describe("Staking contract", function () {
+    beforeEach(async function () {
+        [owner] = await ethers.getSigners();
+
+        // deploy underlying asset
+        const Tus = await ethers.getContractFactory("Tus");
+        tus = await Tus.deploy("1000000000000000000000");
+
+        // deploy security deposit token
+        const SecurityDepositToken = await ethers.getContractFactory("SecurityDepositToken");
+        securityDepositToken = await SecurityDepositToken.deploy(tus.address, 18, "Treasure Under Sea", "TUS");
+
+        const StakingRewards = await ethers.getContractFactory("StakingRewards");
+        stakingRewards = await StakingRewards.deploy(tus.address, securityDepositToken.address);
+
+    });
+
+    it("Deployment should give the deployer the ownership", async function () {
+        expect(await stakingRewards.owner()).to.equal(owner.address);
+    });
+
+    it("New deployed staking contract should have 0 total supply", async  function() {
+        expect(await stakingRewards.totalSupply()).to.equal("0");
+
+    })
+});
