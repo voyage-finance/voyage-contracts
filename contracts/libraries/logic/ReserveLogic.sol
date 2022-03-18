752bdbfff5a06a5e7954638c68f2ef46d0664749
xiaohuo <xiaohuo200@gmail.com>
2022-03-18 14:11:10 +0800

    feat: bootstrap ReserveLogic library

---
+++ contracts/libraries/logic/ReserveLogic.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+import 'openzeppelin-solidity/contracts/utils/math/SafeMath.sol';
+import "../math/WadRayMath.sol";
+
+
+/**
+ * @title ReserveLogic library
+ * @author Voyager
+ * @notice Implements the logic to update the reserves state
+ **/
+library ReserveLogic {
+    using SafeMath for uint256;
+    using WadRayMath for uint256;
+
+
+}
