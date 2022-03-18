71da959de09dd8df2bc8bfce71dfaf9674027715
xiaohuo <xiaohuo200@gmail.com>
2022-03-18 14:03:20 +0800

    feat: define core data structure for liquidity manager

---
+++ contracts/libraries/types/DataTypes.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+library DataTypes {
+    enum Tranche {
+        JUNIOR,
+        SENIOR
+    }
+
+    struct ReserveData {
+        //stores the reserve configuration
+        ReserveConfigurationMap configuration;
+        // for calculating overall interested accumulated
+        // then split it into two indexs base on two allocations
+        uint256 currentOverallLiquidityRate;
+        // Expressed in ray
+        uint256 currentSeniorIncomeAllocation;
+        // Expressed in ray
+        uint256 currentJuniorIncomeAllocation;
+        uint256 currentJuniorLiquidityIndex;
+        uint256 currentSeniorLiquidityIndex;
+        uint256 totalBorrows;
+        uint256 currentBorrowRate;
+        // Expressed in ray
+        uint256 securityRequirement;
+        //the decimals of the reserve asset
+        uint256 decimals;
+        address interestRateStrategyAddress;
+        address jdTokenAddress;
+        address sdTokenAddress;
+        uint40 lastUpdateTimestamp;
+    }
+
+    struct ReserveConfigurationMap {
+        //bit 0-15: Liq. bonus
+        //bit 16-23: Decimals
+        //bit 24: Reserve is active
+        //bit 25: reserve is frozen
+        //bit 26: borrowing is enabled
+        //bit 27-30: reserved
+        //bit 31-46: reserve factor
+        //bit 47-62: lock up period in seconds
+        uint256 data;
+    }
+}
