e1ff57438ef283cde24220e063d7a67769a05220
xiaohuo <xiaohuo200@gmail.com>
2022-03-21 14:08:50 +0800

    refactor(BaseDepositERC20): remove old mint and burn functions

--- contracts/tokenization/BaseDepositERC20.sol
+++ contracts/tokenization/BaseDepositERC20.sol
@@ -79,21 +79,8 @@ contract BaseDepositERC20 is Context, IERC20, IERC20Metadata {
         return _symbol;
     }
 
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless this function is
-     * overridden;
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
     function decimals() public view virtual override returns (uint8) {
-        return 18;
+        return decimals;
     }
 
     /**
@@ -289,39 +276,6 @@ contract BaseDepositERC20 is Context, IERC20, IERC20Metadata {
         emit Transfer(sender, recipient, amount);
 
         _afterTokenTransfer(sender, recipient, amount);
-    }
-
-    /**
-     * @dev Mints stable debt tokens to an user
-     * @param account The account receiving the debt tokens
-     * @param amount The amount being minted
-     * @param oldTotalSupply the total supply before the minting event
-     **/
-    function _mint(
-        address account,
-        uint256 amount,
-        uint256 oldTotalSupply
-    ) internal {
-        uint256 oldAccountBalance = _balances[account];
-        _balances[account] = oldAccountBalance.add(amount);
-    }
-
-    /**
-     * @dev Burns stable debt tokens of an user
-     * @param account The user getting his debt burned
-     * @param amount The amount being burned
-     * @param oldTotalSupply The total supply before the burning event
-     **/
-    function _burn(
-        address account,
-        uint256 amount,
-        uint256 oldTotalSupply
-    ) internal {
-        uint256 oldAccountBalance = _balances[account];
-        _balances[account] = oldAccountBalance.sub(
-            amount,
-            'SDT_BURN_EXCEEDS_BALANCE'
-        );
     }
 
     /**
