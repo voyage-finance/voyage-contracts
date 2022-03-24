bc2f0ce9ea03b7884e64a8145e079b0528018e5c
xiaohuo <xiaohuo200@gmail.com>
2022-03-24 20:23:12 +0800

    feat(error): add three validation failed error

--- contracts/libraries/helpers/Errors.sol
+++ contracts/libraries/helpers/Errors.sol
@@ -6,11 +6,15 @@ pragma solidity ^0.8.9;
  * @author Voyager
  * @notice Defines the error messages emitted by the different contracts of the Voyager protocol
  * @dev Error messages prefix glossary:
+ *  - VL = ValidationLogic
  *  - LM = LiquidityManager
  *  - CT = Common errors between tokens
  *  - RL = ReserveLogic
  */
 library Errors {
+    string public constant VL_INVALID_AMOUNT = '1'; // 'Amount must be greater than 0'
+    string public constant VL_NO_ACTIVE_RESERVE = '2'; // 'Action requires an active reserve'
+    string public constant VL_RESERVE_FROZEN = '3'; // 'Action cannot be performed because the reserve is frozen'
     string public constant CT_CALLER_MUST_BE_LIQUIDITY_MANAGER_POOL = '20';
     string public constant CT_INVALID_MINT_AMOUNT = '21';
     string public constant CT_INVALID_BURN_AMOUNT = '22';
