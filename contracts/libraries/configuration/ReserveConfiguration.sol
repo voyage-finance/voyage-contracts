74d55d1dbfa609d79a344bb9f6cb0bdccec0c9e2
xiaohuo <xiaohuo200@gmail.com>
2022-03-24 20:05:59 +0800

    feat(configuration): get flags: active, frozen and borrowing enabled

--- contracts/libraries/configuration/ReserveConfiguration.sol
+++ contracts/libraries/configuration/ReserveConfiguration.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
+import '../types/DataTypes.sol';
+
 /**
  * @title ReserveConfiguration library
  * @author Voyage
@@ -15,4 +17,27 @@ library ReserveConfiguration {
     uint256 constant RESERVED_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF87FFFFFF; // prettier-ignore
     uint256 constant RESERVE_FACTOR_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA0007FFFFFFF; // prettier-ignore
     uint256 constant LOCKUP_PERIOD_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF80007FFFFFFFFFFF; // prettier-ignore
+
+    /**
+     * Gets the configuration flags of the reserve
+     * @param self The reserve configuration
+     * @return The state flags representing active, frozen, borrowing enabled
+     **/
+    function getFlags(DataTypes.ReserveConfigurationMap storage self)
+        internal
+        view
+        returns (
+            bool,
+            bool,
+            bool
+        )
+    {
+        uint256 dataLocal = self.data;
+
+        return (
+            (dataLocal & ~ACTIVE_MASK) != 0,
+            (dataLocal & ~FROZEN_MASK) != 0,
+            (dataLocal & ~BORROWING_ENABLE_MASK) != 0
+        );
+    }
 }
