e4d62d0af8877c658662fb860034bfe7aecb3bcd
xiaohuo <xiaohuo200@gmail.com>
2022-03-28 23:37:49 +0800

    test: setup liquidity deposit

---
+++ test/liquidity_deposit.js
@@ -0,0 +1,73 @@
+const { expect } = require('chai');
+
+let owner;
+let voyager;
+
+describe('Reserve Init', function () {
+  beforeEach(async function () {
+    [owner] = await ethers.getSigners();
+
+    // deploy Voyager contract
+    const Voyager = await ethers.getContractFactory('Voyager');
+    voyager = await Voyager.deploy(owner.address);
+
+    // deploy AddressResolver contract
+    const AddressResolver = await ethers.getContractFactory('AddressResolver');
+    const addressResolver = await AddressResolver.deploy();
+
+    // set AddressResolver address to Voyager
+    await voyager.setAddressResolverAddress(addressResolver.address);
+
+    // deploy LiquidityManagerProxy contract
+    const LiquidityManagerProxy = await ethers.getContractFactory(
+      'LiquidityManagerProxy'
+    );
+    const liquidityManagerProxy = await LiquidityManagerProxy.deploy();
+
+    // deploy LiquidityManager contract
+    const LiquidityManager = await ethers.getContractFactory(
+      'LiquidityManager'
+    );
+    const liquidityManager = await LiquidityManager.deploy(
+      liquidityManagerProxy.address,
+      voyager.address
+    );
+    liquidityManagerProxy.setTarget(liquidityManager.address);
+
+    // deploy ReserveLogic library
+    const ReserveLogic = await ethers.getContractFactory('ReserveLogic');
+    const reserveLogic = await ReserveLogic.deploy();
+    // deploy ValidationLogic library
+    const ValidationLogic = await ethers.getContractFactory('ValidationLogic');
+    const validationLogic = await ValidationLogic.deploy();
+
+    // deploy LiquidityManagerStorage contract
+    const LiquidityManagerStorage = await ethers.getContractFactory(
+      'LiquidityManagerStorage',
+      {
+        libraries: {
+          ReserveLogic: reserveLogic.address,
+          ValidationLogic: validationLogic.address,
+        },
+      }
+    );
+    const liquidityManagerStorage = await LiquidityManagerStorage.deploy(
+      liquidityManager.address
+    );
+
+    // import vaultManager to AddressResolver
+    const names = [
+      ethers.utils.formatBytes32String('liquidityManagerProxy'),
+      ethers.utils.formatBytes32String('liquidityManagerStorage'),
+    ];
+    const destinations = [
+      liquidityManagerProxy.address,
+      liquidityManagerStorage.address,
+    ];
+
+    await addressResolver.importAddresses(names, destinations);
+
+    await liquidityManagerProxy.transferOwnership(voyager.address);
+    await voyager.claimLiquidityManagerProxyOwnership();
+  });
+});
