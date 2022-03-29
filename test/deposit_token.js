92c4ae34a0f8c8222022a99adb2a2b629b4f7479
xiaohuo <xiaohuo200@gmail.com>
2022-03-29 18:22:10 +0800

    test: initialize twice should revert with error

--- test/deposit_token.js
+++ test/deposit_token.js
@@ -26,4 +26,37 @@ describe('Initialize Deposit Token', function () {
     const name = await juniorDepositToken.name();
     expect(name).to.equal('Treasure Under Sea');
   });
+
+  it('Initialize junior token twice should report error', async function () {
+    // deploy AddressResolver contract
+    const AddressResolver = await ethers.getContractFactory('AddressResolver');
+    const addressResolver = await AddressResolver.deploy();
+
+    const Tus = await ethers.getContractFactory('Tus');
+    const tus = await Tus.deploy('1000000000000000000000');
+
+    const JuniorDepositToken = await ethers.getContractFactory(
+      'JuniorDepositToken'
+    );
+    const juniorDepositToken = await JuniorDepositToken.deploy();
+
+    await juniorDepositToken.initialize(
+      addressResolver.address,
+      tus.address,
+      18,
+      tus.name(),
+      tus.symbol(),
+      ethers.utils.formatBytes32String('')
+    );
+    await expect(
+      juniorDepositToken.initialize(
+        addressResolver.address,
+        tus.address,
+        18,
+        tus.name(),
+        tus.symbol(),
+        ethers.utils.formatBytes32String('')
+      )
+    ).to.be.revertedWith('Contract instance has already been initialized');
+  });
 });
