b37aaf4e3106e1ad742f8e1dfb4d291dabdd4972
xiaohuo <xiaohuo200@gmail.com>
2022-04-04 12:44:56 +0800

    feat(State): support multiple associated contracts

--- contracts/libraries/state/State.sol
+++ contracts/libraries/state/State.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.9;
 import '../ownership/Ownable.sol';
 
 abstract contract State is Ownable {
-    address public associatedContract;
+    mapping(address => bool) public associatedContracts;
 
     constructor(address _associatedContract) {
         // This contract is abstract, and thus cannot be instantiated directly
         require(owner != address(0), 'Owner must be set');
 
-        associatedContract = _associatedContract;
+        associatedContracts[_associatedContract] = true;
         emit AssociatedContractUpdated(_associatedContract);
     }
 
@@ -18,7 +18,7 @@ abstract contract State is Ownable {
 
     modifier onlyAssociatedContract() {
         require(
-            msg.sender == associatedContract,
+            associatedContracts[msg.sender],
             'Only the associated contract can perform this action'
         );
         _;
@@ -29,7 +29,7 @@ abstract contract State is Ownable {
         external
         onlyOwner
     {
-        associatedContract = _associatedContract;
+        associatedContracts[_associatedContract] = true;
         emit AssociatedContractUpdated(_associatedContract);
     }
 }
