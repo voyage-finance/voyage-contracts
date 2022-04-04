a3d1d29e2f8f2b3c92efd6d871c4f94ac520c946
xiaohuo <xiaohuo200@gmail.com>
2022-04-04 17:44:33 +0800

    feat: bootstrap loan manager proxy

--- contracts/component/loan/LoanManager.sol
+++ contracts/component/loan/LoanManager.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-contract LoanManager {}
+import '../../libraries/proxy/Proxyable.sol';
+
+contract LoanManager is Proxyable {
+    constructor(address payable _proxy) Proxyable(_proxy) {}
+}
--- contracts/component/loan/LoanManagerProxy.sol
+++ contracts/component/loan/LoanManagerProxy.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-contract LoanManagerProxy {}
+import '../../libraries/proxy/Proxy.sol';
+
+contract LoanManagerProxy is Proxy {}
