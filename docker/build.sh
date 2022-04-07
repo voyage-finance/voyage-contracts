ce7eb5089a6c88f8c8676c7a069845b7a33dc71d
iantanwx <ian.tan@voyage.finance>
2022-04-07 17:00:43 +0800

    build: change ECR repo name

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -f ./docker/Dockerfile . -t 596511190950.dkr.ecr.us-west-2.amazonaws.com/voyage-finance:latest
+docker build -f ./docker/Dockerfile . -t 596511190950.dkr.ecr.us-west-2.amazonaws.com/voyage-core-contracts:latest
