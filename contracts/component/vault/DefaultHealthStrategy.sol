710da72f9c92362ac172c3815cb1f0f9f4d75fe4
xiaohuo <xiaohuo200@gmail.com>
2022-04-08 13:10:46 +0800

    feat(DefaultHealthStrategy): immutable fields

--- contracts/component/vault/DefaultHealthStrategy.sol
+++ contracts/component/vault/DefaultHealthStrategy.sol
@@ -2,5 +2,26 @@
 pragma solidity ^0.8.9;
 
 import '../../interfaces/IHealthStrategy.sol';
+import 'openzeppelin-solidity/contracts/utils/math/SafeMath.sol';
+import '../../libraries/math/WadRayMath.sol';
 
-contract DefaultHealthStrategy {}
+contract DefaultHealthStrategy is IHealthStrategy {
+    using WadRayMath for uint256;
+    using SafeMath for uint256;
+
+    // A number >= 1 by which loan amount is multiplied. Riskier assets will attract
+    // a higher PF, effectively increasing the expected repayment rate. Expressed in ray
+    uint256 internal immutable premiumFactor;
+
+    uint256 internal immutable loanPeriod;
+
+    constructor(uint256 _premiumFactor, uint256 _loanPeriod) public {
+        premiumFactor = _premiumFactor;
+        loanPeriod = _loanPeriod;
+    }
+
+    function calculateHealthRisk() external view returns (uint256) {
+        //todo
+        //return 1;
+    }
+}
