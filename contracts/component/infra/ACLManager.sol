a54a58d4fb0e9f6c816fb3f5dbac26bb71d1100a
xiaohuo <xiaohuo200@gmail.com>
2022-04-12 22:56:09 +0800

    feat(ACLManager): setup admin role

--- contracts/component/infra/ACLManager.sol
+++ contracts/component/infra/ACLManager.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.9;
 
 import 'openzeppelin-solidity/contracts/access/AccessControl.sol';
-import "../../interfaces/IACLManager.sol";
+import '../../interfaces/IACLManager.sol';
 
-contract ACLManager is AccessControl,IACLManager {
+contract ACLManager is AccessControl, IACLManager {
+    bytes32 public constant LIQUIDITY_MANAGER_ADMIN_ROLE =
+        keccak256('LIQUIDITY_MANAGER_ADMIN');
 
+    constructor(address _aclAdmin) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _aclAdmin);
+    }
 }
--- contracts/interfaces/IACLManager.sol
+++ contracts/interfaces/IACLManager.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-interface IACLManager {
-
-}
+interface IACLManager {}
