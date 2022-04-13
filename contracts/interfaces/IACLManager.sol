6e41d3c662b8b07b56d09ef1da335cf3464110f5
xiaohuo <xiaohuo200@gmail.com>
2022-04-14 06:39:26 +0800

    refactor(IACLManager): complete the interface defination

--- contracts/interfaces/IACLManager.sol
+++ contracts/interfaces/IACLManager.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.9;
 
 interface IACLManager {
+    function grantLiquidityManager(address _admin) external;
+
     function isLiquidityManager(address _admin) external view returns (bool);
 
+    function grantVaultManager(address _admin) external;
+
     function isVaultManager(address _admin) external view returns (bool);
+
+    function grantPoolManager(address _admin) external;
 
     function isProtocolManager(address _admin) external view returns (bool);
 }
