2f8a4863b7c10be21962d3f4cbfe655923598713
xiaohuo <xiaohuo200@gmail.com>
2022-04-13 17:29:49 +0800

    feat(ACLManager): add new role: protocol configurator

--- contracts/component/infra/ACLManager.sol
+++ contracts/component/infra/ACLManager.sol
@@ -5,6 +5,9 @@ import 'openzeppelin-solidity/contracts/access/AccessControl.sol';
 import '../../interfaces/IACLManager.sol';
 
 contract ACLManager is AccessControl, IACLManager {
+    bytes32 public constant PROTOCOL_CONFIGURE_ADMIN_ROLE =
+        keccak256('PROTOCOL_CONFIGURE_ADMIN');
+
     bytes32 public constant LIQUIDITY_MANAGER_ADMIN_ROLE =
         keccak256('LIQUIDITY_MANAGER_ADMIN');
 
@@ -21,5 +24,9 @@ contract ACLManager is AccessControl, IACLManager {
 
     function isVaultManager(address _admin) external view returns (bool) {
         return hasRole(VAULT_MANAGER_ADMIN_ROLE, _admin);
+    }
+
+    function isProtocolConfigure(address _admin) external view returns (bool) {
+        return hasRole(PROTOCOL_CONFIGURE_ADMIN_ROLE, _admin);
     }
 }
