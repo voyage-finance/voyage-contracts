ff449a6a5c652ddfd34bd2f669bb5406c92aac01
xiaohuo <xiaohuo200@gmail.com>
2022-04-23 06:25:37 +0800

    fix: use remaning balance to calculate repayment

--- contracts/tokenization/StableDebtToken.sol
+++ contracts/tokenization/StableDebtToken.sol
@@ -189,7 +189,8 @@ contract StableDebtToken is
                 stableRate,
                 drawDone.timestamp
             );
-            uint256 cumulatedBalance = drawDone.amount.rayMul(
+            uint256 remainingBalance = drawDone.amount - drawDone.repayment.totalPaid;
+            uint256 cumulatedBalance = remainingBalance.rayMul(
                 cumulatedInterest
             );
             aggregateOptimalRepaymentRate += cumulatedBalance.rayDiv(
