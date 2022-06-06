db6e5c61fdb00ceb3479e9248ca615f6c4f289d9
daulet <daulet.issatayev@nu.edu.kz>
2022-06-06 13:57:17 +0600

    feat: return amounts in pendingWithdrawal

---
+++ scripts/0015_withdraw.js
@@ -0,0 +1,29 @@
+const hre = require('hardhat');
+const { deployments, ethers, getNamedAccounts } = hre;
+
+const getAddress = (contract) =>
+  hre.deployments.get(contract).then(({ address }) => address);
+
+async function main() {
+  const { owner } = await getNamedAccounts();
+  const voyager = await ethers.getContract('Voyager', owner);
+  
+  const tus = await deployments.get('Tus');
+
+  await voyager.withdraw(tus.address, '1', '10000000000000000000');
+  
+  /*
+  const SeniorDepositToken = await ethers.getContract('SeniorDepositToken');
+  const [times, amounts] = await SeniorDepositToken.pendingWithdrawal(owner);
+  
+  console.log("pending times", times);
+  console.log("pending amounts", amounts);
+  */
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
