df8d068048fd1d7e63eaa123533b3708d5528a05
iantanwx <ian.tan@voyage.finance>
2022-08-30 15:40:02 +0800

    fix: eip1271 signature validation
    Makes the Voyage implementation consistent with Argent. See : https://github.com/argentlabs/argent-contracts/blob/bd069ac889bfbf88439dd219893e75450926412f/contracts/modules/TransactionManager.sol#L232

--- test/vault_signature.spec.ts
+++ test/vault_signature.spec.ts
@@ -5,23 +5,18 @@ import { setupTestSuite } from '../helpers/setupTestSuite';
 describe('Vault Signature', function () {
   it('Vault should recover correct address from signature', async function () {
     const { owner, voyage } = await setupTestSuite();
-    const vault = await voyage.getVault(owner);
-    const bp = await ethers.getContractFactory('Vault');
-    const vaultInstance = await bp.attach(vault);
-
-    // Create a wallet to sign the message with
-    let privateKey =
-      '0x0123456789012345678901234567890123456789012345678901234567890123';
-    let wallet = new ethers.Wallet(privateKey);
-
-    console.log(wallet.address);
-    let message = 'Hello World';
-    let messageHash = ethers.utils.id(message);
-    let messageHashBytes = ethers.utils.arrayify(messageHash);
-
+    const vaultAddress = await voyage.getVault(owner);
+    const vault = await ethers.getContractAt('Vault', vaultAddress);
     const signer = await ethers.getSigner(owner);
-    const flatSig = await signer.signMessage(messageHashBytes);
-    const result = await vaultInstance.isValidSignature(messageHash, flatSig);
-    // expect(result).to.equal('0x1626ba7e');
+    let message =
+      'Welcome to OpenSea!\n\nClick to sign in and accept the OpenSea Terms of Service: https://opensea.io/tos\n\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n\nWallet address:\n0xad5792b1d998f607d3eeb2f357138a440b03f19f\n\nNonce:\naff63e58-1b2c-44a6-99c1-eb0fbc9dacab';
+    const signature = await signer.signMessage(message);
+    const rec = ethers.utils.verifyMessage(message, signature);
+    expect(rec).to.equal(signer.address);
+    const result = await vault.isValidSignature(
+      ethers.utils.hashMessage(message),
+      signature
+    );
+    expect(result).to.equal('0x1626ba7e');
   });
 });
