5476d6e661e7e83b04deb219093f32bd41d6ff4a
xiaohuo <xiaohuo@voyage.finance>
2022-08-31 15:55:58 +0800

    fix: add outstanding interest to junor tokens's total assets

--- contracts/voyage/tokenization/JuniorDepositToken.sol
+++ contracts/voyage/tokenization/JuniorDepositToken.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.9;
 
 import {VToken} from "./VToken.sol";
+import {LoanFacet} from "../facets/LoanFacet.sol";
 
 contract JuniorDepositToken is VToken {
     function totalAssets() public view override returns (uint256) {
-        return asset.balanceOf(address(this));
+        uint256 outstandingInterest = LoanFacet(voyage).juniorInterestBalance(
+            collection,
+            address(asset)
+        );
+        return asset.balanceOf(address(this)) + outstandingInterest;
     }
 }
