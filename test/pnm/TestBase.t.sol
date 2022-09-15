1abfa0ce87ec9ec3ecdc0589ab679d24f9ddaf07
Conan <conanchou1412@gmail.com>
2022-09-15 18:20:45 -0400

    add function selectors

--- test/pnm/TestBase.t.sol
+++ test/pnm/TestBase.t.sol
@@ -15,6 +15,7 @@ import "contracts/voyage/adapter/SeaportAdapter.sol";
 import "contracts/mock/Crab.sol";
 import {MockMarketPlace} from "contracts/mock/MockMarketplace.sol";
 import {MockSeaport} from "contracts/mock/MockSeaport.sol";
+import {MockForwarder} from "contracts/mock/MockForwarder.sol";
 
 import "contracts/voyage/strategy/DefaultReserveInterestRateStrategy.sol";
 import "contracts/vault/Vault.sol";
@@ -23,6 +24,7 @@ import "contracts/shared/diamond/facets/DiamondLoupeFacet.sol";
 import "contracts/shared/diamond/facets/OwnershipFacet.sol";
 
 import {InitDiamond} from "contracts/voyage/InitDiamond.sol";
+import "contracts/shared/diamond/interfaces/IDiamondCut.sol";
 
 import "contracts/voyage/facets/LiquidityFacet.sol";
 import "contracts/voyage/facets/ConfigurationFacet.sol";
@@ -30,14 +32,12 @@ import "contracts/voyage/facets/DataProviderFacet.sol";
 import {PaymentsFacet} from "contracts/shared/facets/PaymentsFacet.sol";
 import "contracts/voyage/facets/MarketplaceAdapterFacet.sol";
 
-contract TestBase is Agent {
+contract TestBase is Agent, IDiamondCut {
     address owner = address(0x0);
     address alice = address(0x1);
     address bob = address(0x2);
     address treasury = address(0x9);
     address forwarder = address(0xa);
-
-    address internal vault;
 
     uint256 WAD = 10**18;
     uint256 RAY = 10**27;
@@ -45,6 +45,7 @@ contract TestBase is Agent {
     Voyage internal voyage;
     SeniorDepositToken internal seniorDepositToken;
     JuniorDepositToken internal juniorDepositToken;
+    Vault internal vault;
     WETH9 internal weth;
     VoyagePaymaster internal paymaster;
     PriceOracle internal priceOracle;
@@ -54,34 +55,81 @@ contract TestBase is Agent {
     Crab internal crab;
     MockMarketPlace internal mockMarketPlace;
     MockSeaport internal mockSeaport;
+    MockForwarder internal mockForwarder;
     DefaultReserveInterestRateStrategy
         internal defaultReserveInterestRateStrategy;
 
     function deploy() internal {
-        _deploy_voyager();
-        _deploy_external();
-        _setup_test();
+        _deploy000Mock();
+        _deploy001Vtoken();
+        _deploy002Vault();
+        _deploy003MarketplaceAdapter();
+        _deploy004InterestRateStrategy();
+        _deploy005PriceOracle();
+        _deploy006Diamond();
+        _deploy007Facets();
+        _deploy008Paymaster();
     }
 
-    function _deploy_voyager() internal {
-        voyage = new Voyage(owner);
+    function _deploy000Mock() internal {
+        vm.startPrank(owner);
+        weth = new WETH9();
+        mockForwarder = new MockForwarder();
+        
+        crab = new Crab("Mocked Crab", "MC");
+        mockMarketPlace = new MockMarketPlace();
+        mockSeaport = new MockSeaport();
+        vm.stopPrank();
+    }
 
+    function _deploy001Vtoken() internal {
         vm.startPrank(owner);
         seniorDepositToken = new SeniorDepositToken();
         juniorDepositToken = new JuniorDepositToken();
-        Vault vault = new Vault();
-        priceOracle = new PriceOracle();
+        vm.stopPrank();
+    }
+
+    function _deploy002Vault() internal {
+        vm.startPrank(owner);
+        vault = new Vault();
+        vm.stopPrank();
+    }
+
+    function _deploy003MarketplaceAdapter() internal {
+        vm.startPrank(owner);
         looksRareAdapter = new LooksRareAdapter();
-        DiamondCutFacet diamondCutFacet = new DiamondCutFacet();
-        DiamondLoupeFacet diamondLoupeFacet = new DiamondLoupeFacet();
-        OwnershipFacet ownershipFacet = new OwnershipFacet();
-        weth = new WETH9();
         seaportAdapter = new SeaportAdapter(address(weth));
-        paymaster = new VoyagePaymaster(
-            address(voyage),
-            address(weth),
-            treasury
+        vm.stopPrank();
+    }
+
+    function _deploy004InterestRateStrategy() internal {
+        vm.startPrank(owner);
+        uint256 utilisationRate = (8 * RAY) / 10;
+        uint256 slope = (4 * RAY) / 100;
+        uint256 baseInterest = (18 * RAY) / 100;
+
+        defaultReserveInterestRateStrategy = new DefaultReserveInterestRateStrategy(
+            utilisationRate,
+            slope,
+            baseInterest
         );
+        vm.stopPrank();
+    }
+
+    function _deploy005PriceOracle() internal {
+        vm.startPrank(owner);
+        priceOracle = new PriceOracle();
+        vm.stopPrank();
+    }
+
+    function _deploy006Diamond() internal {
+        vm.startPrank(owner);
+        voyage = new Voyage(owner);
+        vm.stopPrank();
+    }
+
+    function _deploy007Facets() internal {
+        vm.startPrank(owner);
 
         InitDiamond initDiamond = new InitDiamond();
 
@@ -93,56 +141,88 @@ contract TestBase is Agent {
         DataProviderFacet dataProviderFacet = new DataProviderFacet();
         PaymentsFacet paymentsFacet = new PaymentsFacet();
         MarketplaceAdapterFacet marketplaceAdapterFacet = new MarketplaceAdapterFacet();
-        vm.stopPrank();
 
-        bytes4[] memory functionSelectors;
-        IDiamondCut.FacetCut[8] memory diamondCuts = [
-            IDiamondCut.FacetCut(address(securityFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(liquidityFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(loanFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(vaultFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(configurationFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(dataProviderFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(paymentsFacet), IDiamondCut.FacetCutAction.Add, functionSelectors),
-            IDiamondCut.FacetCut(address(marketplaceAdapterFacet), IDiamondCut.FacetCutAction.Add, functionSelectors)
+        FacetCut[] memory diamondCut = [
+            FacetCut({
+                facetAddress: address(securityFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("SecurityFacet")
+            }),
+            FacetCut({
+                facetAddress: address(liquidityFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("LiquidityFacet")
+            }),
+            FacetCut({
+                facetAddress: address(loanFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("LoanFacet")
+            }),
+            FacetCut({
+                facetAddress: address(vaultFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("VaultFacet")
+            }),
+            FacetCut({
+                facetAddress: address(configurationFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("ConfigurationFacet")
+            }),
+            FacetCut({
+                facetAddress: address(dataProviderFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("DataProviderFacet")
+            }),
+            FacetCut({
+                facetAddress: address(paymentsFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("PaymentsFacet")
+            }),
+            FacetCut({
+                facetAddress: address(marketplaceAdapterFacet), 
+                action: FacetCutAction.Add, 
+                functionSelectors: _generateSelectors("MarketplaceAdapterFacet")
+           } )
         ];
 
-        voyage.diamondCut(
-            diamondCuts,
+        IDiamondCut(address(voyage)).diamondCut(
+            diamondCut,
             address(initDiamond),
             initDiamond.init(
-                InitDiamond.Args(
-                    address(owner),
-                    address(seniorDepositToken),
-                    address(juniorDepositToken),
-                    address(vault),
-                    address(diamondCutFacet),
-                    address(diamondLoupeFacet),
-                    address(ownershipFacet),
-                    address(weth),
-                    address(forwarder),
-                    address(paymaster)
-                )
+                InitDiamond.Args({
+                    initOwner: address(owner),
+                    seniorDepositTokenImpl: address(seniorDepositToken),
+                    juniorDepositTokenImpl: address(juniorDepositToken),
+                    vaultImpl: address(vault),
+                    weth9: address(weth)
+                })
             )
         );
+        vm.stopPrank();
     }
 
-    function _deploy_external() internal {
+    function _deploy008Paymaster() internal {
         vm.startPrank(owner);
-        crab = new Crab("Mock Crab", "MC");
-        mockMarketPlace = new MockMarketPlace();
-        mockSeaport = new MockSeaport();
-
-        uint256 utilisationRate = (8 * RAY) / 10;
-        uint256 slope = (4 * RAY) / 100;
-        uint256 baseInterest = (18 * RAY) / 100;
-
-        defaultReserveInterestRateStrategy = new DefaultReserveInterestRateStrategy(
-            utilisationRate,
-            slope,
-            baseInterest
+        paymaster = new VoyagePaymaster(
+            address(voyage),
+            address(weth),
+            treasury
         );
+        paymaster.setTrustedForwarder(forwarder);
+        paymaster.setRelayHub(forwarder);
         vm.stopPrank();
+    }
+
+    function _generateSelectors(string memory _facetName)
+        internal
+        returns (bytes4[] memory selectors)
+    {
+        string[] memory cmd = new string[](3);
+        cmd[0] = "node";
+        cmd[1] = "scripts/genSelectors.js";
+        cmd[2] = _facetName;
+        bytes memory res = vm.ffi(cmd);
+        selectors = abi.decode(res, (bytes4[]));
     }
 
     function _setup_test() internal {
@@ -191,7 +271,10 @@ contract TestBase is Agent {
         // the "todo delete" section, won't transcribe it till we need it.
     }
 
-    function _make_cut(address facet) internal returns (IDiamondCut.FacetCut memory) {
-        
-    }
+    function diamondCut(
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external override {}
+
 }
