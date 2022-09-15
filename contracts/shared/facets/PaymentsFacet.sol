60af807efe08a18e486ee87856eab9c56ce9bdda
xiaohuo <xiaohuo@voyage.finance>
2022-09-15 14:41:01 +0800

    fix: make PaymentsFacet only called by voyage itself

--- contracts/shared/facets/PaymentsFacet.sol
+++ contracts/shared/facets/PaymentsFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {PeripheryPayments} from "../util/PeripheryPayments.sol";
+import {LibPeripheryPayments} from "../libraries/LibPeripheryPayments.sol";
 import {Multicall} from "../util/Multicall.sol";
 import {SelfPermit} from "../util/SelfPermit.sol";
 import {LibAppStorage} from "../../voyage/libraries/LibAppStorage.sol";
@@ -9,12 +9,18 @@ import {SafeTransferLib} from "../libraries/SafeTransferLib.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract PaymentsFacet is PeripheryPayments, Multicall, SelfPermit {
+contract PaymentsFacet is Multicall, SelfPermit {
     using SafeERC20 for IERC20;
+
+    modifier auth() {
+        require(msg.sender == address(this), "Unauthorised");
+        _;
+    }
 
     function unwrapWETH9(uint256 amountMinimum, address recipient)
         public
         payable
+        auth
     {
         uint256 balanceWETH9 = LibAppStorage.ds().WETH9.balanceOf(
             address(this)
@@ -27,7 +33,7 @@ contract PaymentsFacet is PeripheryPayments, Multicall, SelfPermit {
         }
     }
 
-    function wrapWETH9() public payable {
+    function wrapWETH9() public payable auth {
         if (address(this).balance > 0)
             LibAppStorage.ds().WETH9.deposit{value: address(this).balance}(); // wrap everything
     }
@@ -36,7 +42,7 @@ contract PaymentsFacet is PeripheryPayments, Multicall, SelfPermit {
         IERC20 token,
         uint256 amountMinimum,
         address recipient
-    ) public payable {
+    ) public payable auth {
         uint256 balanceToken = token.balanceOf(address(this));
         require(balanceToken >= amountMinimum, "Insufficient token");
 
@@ -45,10 +51,19 @@ contract PaymentsFacet is PeripheryPayments, Multicall, SelfPermit {
         }
     }
 
-    function refundETH() external payable {
+    function refundETH() external payable auth {
         if (address(this).balance > 0) {
             SafeTransferLib.safeTransferETH(msg.sender, address(this).balance);
         }
+    }
+
+    function pullToken(
+        IERC20 token,
+        uint256 amount,
+        address from,
+        address recipient
+    ) external auth {
+        LibPeripheryPayments.pullToken(token, amount, from, recipient);
     }
 }
 
@@ -59,3 +74,5 @@ abstract contract IWETH9 is IERC20 {
     /// @notice Withdraw wrapped ether to get ether
     function withdraw(uint256) external virtual;
 }
+
+error Unauthorised();
