7cfd7e91729d52fc12581de4cfc8d01c313d1788
xiaohuo <xiaohuo@voyage.finance>
2022-09-17 21:47:15 +0800

    fix: remove unused code

--- contracts/voyage/libraries/LibMarketplace.sol
+++ contracts/voyage/libraries/LibMarketplace.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IVault} from "../../vault/Vault.sol";
 import {IMarketPlaceAdapter, AssetInfo} from "../interfaces/IMarketPlaceAdapter.sol";
 import {Storage, LibAppStorage} from "./LibAppStorage.sol";
