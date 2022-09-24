d71f22cb50a30aba52a5d8e45d0b6b71df2331e4
iantanwx <ian.tan@voyage.finance>
2022-09-24 12:36:53 +0800

    build: report deployment gas usage

--- tasks/migration/voyage.dev.ts
+++ tasks/migration/voyage.dev.ts
@@ -1,16 +1,30 @@
 import { log } from '@helpers/logger';
-import { task } from 'hardhat/config';
+import { task, types } from 'hardhat/config';
 
-task(
-  'deploy:dev',
-  'Deploys a development environment, including mocks'
-).setAction(async (_, hre) => {
-  await hre.run('set-hre');
-  log.info('Starting migration.');
-  await hre.run('deploy');
+task('deploy:dev', 'Deploys a development environment, including mocks')
+  .addOptionalParam(
+    'tags',
+    'Comma-separated tags to pass to hardhat-deploy',
+    undefined,
+    types.string
+  )
+  .setAction(async (params, hre) => {
+    await hre.run('set-hre');
+    log.info('Starting migration.');
+    await hre.run('deploy', {
+      ...params,
+      reportGas: true,
+    });
+    const gasUsed = hre.deployments.getGasUsed();
+    const deploymentCost = hre.ethers.utils
+      .parseUnits('5', 'gwei')
+      .mul(gasUsed);
+    log.info(
+      `Cost of deployment: ${hre.ethers.utils.formatEther(deploymentCost)} ETH`
+    );
 
-  log.info('Configuring marketplace adapters');
-  await hre.run('dev:configure-marketplace-adapters');
+    log.info('Configuring marketplace adapters');
+    await hre.run('dev:configure-marketplace-adapters');
 
   log.info('Configuring Vault implementation');
   await hre.run('dev:configure-vault-impl');
