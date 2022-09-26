75ac21d9e7cbd558b10685f34f351fd76f614267
tarafans <tarafans7@gmail.com>
2022-09-26 17:31:46 -0400

    Add comment

--- test/pnm/TestSeniorDepositToken.t.sol
+++ test/pnm/TestSeniorDepositToken.t.sol
@@ -4,6 +4,8 @@ import "./TestBase.t.sol";
 
 // This is a testing of the vault used by Voyage.
 contract TestSeniorDepositToken is TestBase {
+    // Shared setUp() process in TestBase
+
     function check() public {
         // Unbonding assets are the assets that are requested to be withdrawn and thus frozen.
         // The amount of unbounding assets should never be larger than the actual assets stored in the vault. 
