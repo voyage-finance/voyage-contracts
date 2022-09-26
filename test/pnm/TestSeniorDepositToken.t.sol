c212acbd66cdf22965659458c29237fd1c272cb3
tarafans <tarafans7@gmail.com>
2022-09-26 17:30:45 -0400

    Add invariant for the bug we find

---
+++ test/pnm/TestSeniorDepositToken.t.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.9;
+
+import "./TestBase.t.sol";
+
+// This is a testing of the vault used by Voyage.
+contract TestSeniorDepositToken is TestBase {
+    function check() public {
+        // Unbonding assets are the assets that are requested to be withdrawn and thus frozen.
+        // The amount of unbounding assets should never be larger than the actual assets stored in the vault. 
+        require(seniorDepositToken.totalUnbondingAsset() <= seniorDepositToken.totalAssets());
+    }
+}
