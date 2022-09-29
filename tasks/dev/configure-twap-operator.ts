aa2b88ef277b6d0fcbb3d1ef4778816f49904bc2
iantanwx <ian.tan@voyage.finance>
2022-09-29 15:26:26 +0800

    fix: permissions scripts

--- tasks/dev/configure-twap-operator.ts
+++ tasks/dev/configure-twap-operator.ts
@@ -1,17 +1,14 @@
 import { PriceOracle } from '@contracts';
-import { TWAP_OPERATOR_ADDRESS } from '@helpers/constants';
 import { task } from 'hardhat/config';
 
 task('dev:configure-twap-operator', 'Sets operator for twap')
-  .addOptionalParam('operator', 'The operator address.')
+  .addParam('operator', 'The operator address.')
   .setAction(async (params, hre) => {
     await hre.run('set-hre');
     const { ethers } = hre;
-    const {
-      relayer = TWAP_OPERATOR_ADDRESS
-    } = params;
+    const { operator } = params;
     const priceOracle = await ethers.getContract<PriceOracle>('PriceOracle');
-    const tx = await priceOracle.setOperator(relayer, true);
+    const tx = await priceOracle.setOperator(operator, true);
     await tx.wait();
-    console.log('operator set: ', relayer);
+    console.log('operator set: ', operator);
   });
