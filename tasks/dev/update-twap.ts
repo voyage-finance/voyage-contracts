62de4a9ad5e51b0505520f1e7a5a87af45c8cb58
xiaohuo <xiaohuo@voyage.finance>
2022-10-10 20:21:57 +0800

    script: update twap script

---
+++ tasks/dev/update-twap.ts
@@ -0,0 +1,14 @@
+import { PriceOracle } from '@contracts';
+import { task } from 'hardhat/config';
+
+task('dev:update-twap', 'Update twap')
+  .addParam('currency', 'The currency address.')
+  .addParam('priceAverage', 'The average price.')
+  .setAction(async (params, hre) => {
+    await hre.run('set-hre');
+    const { ethers } = hre;
+    const { currency,priceAverage } = params;
+    const priceOracle = await ethers.getContract<PriceOracle>('PriceOracle');
+    const tx = await priceOracle.updateTwap(currency, priceAverage);
+    await tx.wait();
+  });
